6f04b11431967729ab9f3483463bb91260cd35e6
vForVengeance <peppemarchese97@gmail.com>
2019-04-12 11:38:44 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require ('express');
+var app = express();
+var cors = require('cors')
+//var read = require ("./routes/read.js");
+//var modify = require ("./routes/modify.js");
+var bodyParser = require('body-parser');
+
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+//app.use("/", read);
+//app.use("/modify", modify);
+
+app.listen(3000);
