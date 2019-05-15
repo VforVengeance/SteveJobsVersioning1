8732e4d7e5ba1e8f9f6d99c47734d10f37a46728
vForVengeance <peppemarchese97@gmail.com>
2019-05-15 10:23:22 +0200

    api show

--- routes/read.js
+++ routes/read.js
@@ -10,4 +10,12 @@ router.get('/', function(req, res, next) {
     });
 })
 
+router.get('/:id', function(req, res, next) {
+  User.findOne({_id: req.params.id},function(err, user){
+    if (err) return res.status(500).json({error: err});
+    if(!user) return res.status(404).json({message: 'Utente non trovato'})
+    res.json(user);
+  });
+})
+
 module.exports = router
